d6c9f7e266f3db1d548865131c3e92be76597c01
Abdullah <abdullah.sertkaya@student.hogent.be>
2019-10-13 19:44:45 +0200

    key shit

--- FlightAppAPI/obj/Debug/netcoreapp2.2/FlightAppAPI.AssemblyInfo.cs
+++ FlightAppAPI/obj/Debug/netcoreapp2.2/FlightAppAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a1e0e3de-441f-4d10-a7da-aa5e1d969b98")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
